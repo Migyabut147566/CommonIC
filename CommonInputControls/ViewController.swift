3f0e4e1c02f9876cee7d29c9379e9496b8498b50
Migy Yabut <jmaryabut@gmail.com>
2020-10-20 10:24:15 +0800

    Initial Commit

---
+++ CommonInputControls/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CommonInputControls
+//
+//  Created by Migy Yabut on 10/20/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
